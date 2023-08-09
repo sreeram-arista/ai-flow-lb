a476cdb368e17c684b943867d23078f274a27277
Sreeram Ramachandran <sreeram@arista.com>
2023-08-09 15:40:18 -0700

    Update license text

--- src/include/api.h
+++ src/include/api.h
@@ -1,5 +1,6 @@
-// Copyright (c) 2023 Arista Networks, Inc.  All rights reserved.
-// Arista Networks, Inc. Confidential and Proprietary.
+// Copyright (c) 2023 Arista Networks, Inc.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the COPYING file.
 
 #ifndef CLB_API_H
 #define CLB_API_H
